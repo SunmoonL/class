3eb12f565e5323c8f9e8aa902f92bc36fbfc476d
SunmoonL <dltjsans123@naver.com>
2023-02-21 17:59:45 +0900

    23.02.21

--- js/sub.js
+++ js/sub.js
@@ -1,3 +1,4 @@
+//인트로 슬라이드
 $(function(){
     $(".next").click(function(){
         $("#intro-slide").stop().animate({marginLeft:-165},400,function(){
@@ -11,3 +12,28 @@ $(function(){
         $('#intro-slide').stop().animate({ marginLeft: 0 }, 400);
     });
 });
+
+//사이트맵
+
+$(function(){
+    $("#site > img").click(function(){
+        $("#site_map").toggle();
+    });
+    $("#site_map > a").click(function(){
+        $("#site_map").hide();
+    });
+});
+
+
+$.ajax({
+    method: "GET",
+    url: "https://dapi.kakao.com/v3/search/book?target=title",
+    data: { query: "고양이1" },
+    headers: { Authorization: "KakaoAK 4e5b4da2542e9d9b07f53f75040d87b3" }
+})
+    .done(function (msg) {
+        $(".author > a").prepend(msg.documents[2].authors);
+        $(".translators > a").prepend(msg.documents[2].translators);
+        $(".publisher > a").prepend(msg.documents[2].publisher);
+        $(".datetime").append(msg.documents[2].datetime);
+    });
